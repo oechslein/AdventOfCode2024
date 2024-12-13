c788a846c0dca0508e5100118777c56ad30ac766
Christoph Oechslein <christoph.oechslein@garmin.com>
2024-12-13 15:38:42 +0100

    day13 final
    Change-Id: Ifb00bb3456bc6757ec786b2ff31d7238a41e5d4c

--- day13/src/bin/day13_part2.rs
+++ day13/src/bin/day13_part2.rs
@@ -13,8 +13,8 @@ fn main() -> miette::Result<()> {
     #[cfg(not(feature = "dhat-heap"))]
     tracing_subscriber::fmt::init();
 
-    let file = include_str!("../../input2.txt");
-    let result = process(file).context("process part 2")?;
-    println!("{}", result);
+    let file = include_str!("../../input2.txt").replace('\r', "");
+    let result = process(&file).context("process part 2")?;
+    println!("{result}");
     Ok(())
 }
--- day13/src/day13_part2.rs
+++ day13/src/day13_part2.rs
@@ -1,34 +1,62 @@
-use itertools::Itertools;
 use crate::custom_error::AocError;
+use itertools::Itertools;
 use nalgebra::{Matrix2, Vector2};
 
 #[allow(clippy::cast_sign_loss)]
 #[allow(clippy::cast_precision_loss)]
-fn solve_system(
-    prize_x: i128,
-    prize_y: i128,
-    a_x: i128,
-    a_y: i128,
-    b_x: i128,
-    b_y: i128,
-) -> Option<(i128, i128)> {
-    let coeff_matrix = Matrix2::new(a_x as f64, b_x as f64, a_y as f64, b_y as f64);
-    let constants = Vector2::new(prize_x as f64, prize_y as f64);
-    let solution = coeff_matrix.lu().solve(&constants)?;
-    let button_a = solution[0].round() as i128;
-    let button_b = solution[1].round() as i128;
-
-    ((prize_x == button_a * a_x + button_b * b_x)
-        && (prize_y == button_a * a_y + button_b * b_y)).then_some((button_a, button_b))
+fn solve_system(prize_vector: Vector2<u64>, coeff_matrix: Matrix2<u64>) -> Option<Vector2<u64>> {
+    /*
+     prize_x = a_x * button_a + b_x * button_b
+     prize_y = a_y * button_a + b_y * button_b
+     =>
+     [ prize_x ] = (a_x, b_x ] * [ button_a ]
+     [ prize_y ] = (a_y, b_y ] * [ button_b ]
+     =>
+     [ button_a ] = (a_x, b_x ]^-1 * [ prize_x ]
+     [ button_b ] = (a_y, b_y ]^-1 * [ prize_y ]
+    */
+    // need floats to calculate the inverse
+    let coeff_matrix_float = coeff_matrix.map(|elem| elem as f64);
+    coeff_matrix_float
+        .try_inverse()
+        .and_then(|coeff_matrix_float_inverse| {
+            let prize_vector_float = prize_vector.map(|elem| elem as f64);
+            let solution_float = coeff_matrix_float_inverse * prize_vector_float;
+            let solution = solution_float.map(|x| x.round() as u64);
+            // need to check if we have an integer solution
+            if coeff_matrix * solution == prize_vector {
+                Some(solution)
+            } else {
+                //println!("{} != {}", coeff_matrix * solution, prize_vector);
+                None
+            }
+        })
 }
 
-
+#[allow(clippy::cast_sign_loss)]
+#[allow(clippy::cast_precision_loss)]
 //#[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    let var_name = "\r";
-    let input = input.replace(var_name, "");
+    let additional_price_vec = Vector2::new(10_000_000_000_000, 10_000_000_000_000);
+    let result = parse_input(input).map(|(prize_vec, coeff_matrix)| {
+        if let Some(button_vec) = solve_system(prize_vec + additional_price_vec, coeff_matrix) {
+            let costs = button_vec[0] * 3 + button_vec[1];
+            println!("A/B: {button_vec} => {costs}");
+            costs
+        } else {
+            println!("No solution");
+            0
+        }
+    }).sum::<u64>();
+
+    Ok(result.to_string())
+}
+
+#[allow(clippy::cast_sign_loss)]
+#[allow(clippy::cast_precision_loss)]
+fn parse_input(input: &str) -> impl Iterator<Item = (Vector2<u64>, Matrix2<u64>)> + '_ {
     let input = input.split("\n\n").map(|block| {
-        /* block is eg
+        /* block is for example
         Button A: X+94, Y+34
         Button B: X+22, Y+67
         Prize: X=8400, Y=5400
@@ -44,43 +72,26 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
 
         let (a_x, a_y) = a
             .split(", ")
-            .map(|x| x.split('+').nth(1).unwrap().parse::<i128>().unwrap())
+            .map(|x| x.split('+').nth(1).unwrap().parse::<u64>().unwrap())
             .collect_tuple()
             .unwrap();
         let (b_x, b_y) = b
             .split(", ")
-            .map(|x| x.split('+').nth(1).unwrap().parse::<i128>().unwrap())
+            .map(|x| x.split('+').nth(1).unwrap().parse::<u64>().unwrap())
             .collect_tuple()
             .unwrap();
         let (prize_x, prize_y) = prize
             .split(", ")
-            .map(|x| x.split('=').nth(1).unwrap().parse::<i128>().unwrap())
+            .map(|x| x.split('=').nth(1).unwrap().parse::<u64>().unwrap())
             .collect_tuple()
             .unwrap();
 
-        (
-            (a_x, a_y),
-            (b_x, b_y),
-            (10_000_000_000_000 + prize_x, 10_000_000_000_000 + prize_y),
-        )
+        let coeff_matrix = Matrix2::new(a_x, b_x, a_y, b_y);
+        let prize_vec = Vector2::new(prize_x, prize_y);
+
+        (prize_vec, coeff_matrix)
     });
-
-    let mut result = 0;
-    for ((a_x, a_y), (b_x, b_y), (prize_x, prize_y)) in input {
-        if let Some((button_a, button_b)) = solve_system(prize_x, prize_y, a_x, a_y, b_x, b_y) {
-            println!(
-                "A: {}, B: {} => {}",
-                button_a,
-                button_b,
-                button_a * 3 + button_b
-            );
-            result += button_a * 3 + button_b;
-        } else {
-            println!("No solution");
-        }
-    }
-
-    Ok(result.to_string())
+    input
 }
 
 #[cfg(test)]
@@ -104,14 +115,14 @@ Prize: X=7870, Y=6450
 Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279";
-        assert_eq!("875318608908", process(input)?);
+        assert_eq!("875318608908", process(&input.replace('\r', ""))?);
         Ok(())
     }
 
     #[test]
     fn test_part2() -> miette::Result<()> {
         let input = include_str!("../input2.txt");
-        assert_eq!("99968222587852", process(input)?);
+        assert_eq!("99968222587852", process(&input.replace('\r', ""))?);
         Ok(())
     }
 }
