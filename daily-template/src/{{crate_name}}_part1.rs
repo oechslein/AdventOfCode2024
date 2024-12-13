ea5acc5896a03f2542eb6d75c0dbfae99dd7f7dd
Christoph Oechslein <christoph.oechslein@garmin.com>
2024-12-13 15:38:58 +0100

    Adjusted daily templates
    Change-Id: I0c9617dcfab7b77aa12a96d5fa83b1c61d8a45e0

--- daily-template/src/{{crate_name}}_part1.rs
+++ daily-template/src/{{crate_name}}_part1.rs
@@ -7,7 +7,7 @@ use crate::custom_error::AocError;
 
 //#[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
-    Ok(42.to_string())
+    Ok(input.to_string())
 }
 
 #[cfg(test)]
@@ -17,14 +17,14 @@ mod tests {
     #[test]
     fn test_part1_example() -> miette::Result<()> {
         let input = "";
-        assert_eq!("", process(input)?);
+        assert_eq!("", process(&input.replace('\r', ""))?);
         Ok(())
     }
 
     #[test]
     fn test_part1() -> miette::Result<()> {
         let input = include_str!("../input1.txt");
-        assert_eq!("", process(input)?);
+        assert_eq!("", process(&input.replace('\r', ""))?);
         Ok(())
     }
 }
