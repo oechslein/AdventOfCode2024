4811455dfd7b99e2ca70229ecbea36d8277917c9
Christoph Oechslein <christoph.oechslein@garmin.com>
2024-12-18 11:39:46 +0100

    day18 speedup
    Change-Id: I69ab649ce78653932b9e3a3cd62e279e2383da77

--- day18/src/day18_part2.rs
+++ day18/src/day18_part2.rs
@@ -1,8 +1,8 @@
 use fxhash::{FxHashMap, FxHashSet};
 use grid::{
-    grid_array::{GridArray, GridArrayBuilder},
     grid_hashmap::GridHashMap,
-    grid_types::{Neighborhood, Topology, UCoor2D},
+    grid_iteration::adjacent_cell,
+    grid_types::{Direction, Neighborhood, Topology, UCoor2D},
 };
 use itertools::Itertools;
 use num_traits::ToPrimitive;
@@ -14,62 +14,89 @@ use miette::{miette, Error, Result};
 //#[tracing::instrument]
 pub fn process(input: &str, width: usize) -> std::result::Result<String, Error> {
     //let width = 6+1;
-    let mut grid = GridArrayBuilder::default().topology(Topology::Bounded).neighborhood(
-        Neighborhood::Orthogonal).width(width).height(width).build().unwrap();
+    let mut lower_end = 0;
+    let mut upper_end = input.lines().count();
+    let mut previous_mid = 0;
 
-    for x in 0..width {
-        for y in 0..width {
-            grid.set(x, y, '.');
-        }
-    }
+    let wall_coors = parse(input);
 
-    let lines = input.lines().collect_vec();
+    loop {
+        let mid = (lower_end + upper_end) / 2;
+        //println!("[{lower_end}-{mid}-{upper_end}]");
 
-    let mut last_entry = "";
-    for (index, line) in lines.iter().enumerate() {
-        let (x_str, y_str) = line.split_once(',').unwrap();
-        grid.set(
-            x_str.parse::<usize>().unwrap(),
-            y_str.parse::<usize>().unwrap(),
-            '#',
-        );
-        //grid.println(false);
-
-        let start_coor = UCoor2D::new(0, 0);
-
-        let result = astar(
-            &start_coor,
-            |coor| successors(coor, &grid),
-            |coor| heuristic(coor, &grid),
-            |coor| success(coor, &grid),
-        );
-        //println!("{:?} {:?} {:?}", bytes_to_take, input.lines().nth(bytes_to_take-1).unwrap(), result.clone().and_then(|r| Some(r.1)));
-        if result.is_none() {
-            last_entry = input.lines().nth(index).unwrap();
+        if find_path(width, mid, &wall_coors) {
+            lower_end = mid;
+        } else {
+            upper_end = mid;
+        };
+        if previous_mid == mid {
             break;
         }
+        previous_mid = mid;
     }
-    //let min_costs = &result.unwrap().1;
-    Ok(last_entry.to_string())
+
+    let result = input.lines().nth(previous_mid).unwrap();
+    Ok(result.to_string())
 }
 
-fn successors(coor: &UCoor2D, grid: &GridArray<char>) -> Vec<(UCoor2D, usize)> {
-    grid.neighborhood_cells(coor.x, coor.y)
-        .filter(|(_coor, cell)| cell != &&'#')
-        .map(|(coor, _cell)| (coor, 1))
-        .collect_vec()
+fn parse(input: &str) -> Vec<UCoor2D> {
+    input
+        .lines()
+        .map(|line| {
+            let (x_str, y_str) = line.split_once(',').unwrap();
+
+            UCoor2D::new(
+                x_str.parse::<usize>().unwrap(),
+                y_str.parse::<usize>().unwrap(),
+            )
+        })
+        .collect()
 }
 
-fn success(coor: &UCoor2D, grid: &GridArray<char>) -> bool {
-    *coor == end_coor(grid)
+fn create_wall_set(bytes_to_take: usize, wall_coors: &[UCoor2D]) -> FxHashSet<UCoor2D> {
+    wall_coors.iter().take(bytes_to_take).cloned().collect()
 }
 
-fn end_coor(grid: &GridArray<char>) -> UCoor2D {
-    UCoor2D::new(grid.width() - 1, grid.height() - 1)
+fn find_path(width: usize, bytes_to_take: usize, wall_coors: &[UCoor2D]) -> bool {
+    let wall_set: FxHashSet<&UCoor2D> = wall_coors.iter().take(bytes_to_take).collect();
+    let result = astar(
+        &UCoor2D::new(0, 0),
+        |coor| successors_wall_set(coor, width, &wall_set),
+        |coor| heuristic_wall_set(coor, width),
+        |coor| success_wall_set(coor, width),
+    );
+    //println!("{:?} {:?} {:?}", bytes_to_take, input.lines().nth(bytes_to_take-1).unwrap(), result.clone().and_then(|r| Some(r.1)));
+    result.is_some()
 }
 
-fn heuristic(coor: &UCoor2D, grid: &GridArray<char>) -> usize {
-    coor.manhattan_distance(&end_coor(grid))
+fn successors_wall_set(
+    coor: &UCoor2D,
+    width: usize,
+    wall_set: &FxHashSet<&UCoor2D>,
+) -> Vec<(UCoor2D, usize)> {
+    vec![
+        Direction::West,
+        Direction::East,
+        Direction::South,
+        Direction::North,
+    ]
+    .into_iter()
+    .filter_map(|dir| adjacent_cell(Topology::Bounded, width, width, coor.clone(), dir))
+    .filter(|new_coor| !wall_set.contains(new_coor))
+    .map(|new_coor| (new_coor, 1))
+    .collect()
+}
+
+fn success_wall_set(coor: &UCoor2D, width: usize) -> bool {
+    *coor == end_coor_wall_set(width)
+}
+
+fn end_coor_wall_set(width: usize) -> UCoor2D {
+    UCoor2D::new(width - 1, width - 1)
+}
+
+fn heuristic_wall_set(coor: &UCoor2D, width: usize) -> usize {
+    coor.manhattan_distance(&end_coor_wall_set(width))
 }
 
 #[cfg(test)]
@@ -103,14 +130,14 @@ mod tests {
 0,5
 1,6
 2,0";
-        assert_eq!("6,1", process(&input.replace('\r', ""), 6+1)?);
+        assert_eq!("6,1", process(&input.replace('\r', ""), 6 + 1)?);
         Ok(())
     }
 
     #[test]
     fn test_input() -> miette::Result<()> {
         let input = include_str!("../input2.txt");
-        assert_eq!("60,37", process(&input.replace('\r', ""), 70+1)?);
+        assert_eq!("60,37", process(&input.replace('\r', ""), 70 + 1)?);
         Ok(())
     }
 }
