6bd7405ef7d64ce001221eca775e0d2af0b516e6
Christoph Oechslein <christoph.oechslein@garmin.com>
2024-12-20 09:18:57 +0100

    day19 rwlock

--- day19/src/cache_it_macro.rs
+++ day19/src/cache_it_macro.rs
@@ -3,13 +3,13 @@ macro_rules! cache_it {
     ($cache_type:ty, $cache_init:expr, $key:expr, $expression:expr) => {{
         // LazyLock to initialize a static Mutex<FxHashMap<String, usize>>
         // Mutex to lock the FxHashMap because of the Rayon parallel iterator
-        static CACHE: std::sync::LazyLock<std::sync::Mutex<$cache_type>> =
-            std::sync::LazyLock::new(|| std::sync::Mutex::new($cache_init));
+        static CACHE: std::sync::LazyLock<std::sync::RwLock<$cache_type>> =
+            std::sync::LazyLock::new(|| std::sync::RwLock::new($cache_init));
 
         let key = ($key);
         // Lock the cache in this block
         {
-            let cache = CACHE.lock().unwrap();
+            let cache = CACHE.read().unwrap();
             if let Some(value) = cache.get(&key) {
                 return *value;
             }
@@ -19,7 +19,7 @@ macro_rules! cache_it {
 
         // Lock the cache in this block
         {
-            let mut cache = CACHE.lock().unwrap();
+            let mut cache = CACHE.write().unwrap();
             cache.insert(key, value);
 
             value
